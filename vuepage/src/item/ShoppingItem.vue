c26529469815fceb6087051364dc9904139388c8
aquamarine5 <3168287806@qq.com>
2024-10-13 23:10:34 +0800

    init

---
+++ vuepage/src/item/ShoppingItem.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="shopping_item">
+        <img class="shopping_img">
+        <div class="shopping_content">
+            <div class="shopping_text">
+                <div class="shopping_title">
+
+                </div>
+                <div class="shopping_detail">
+
+                </div>
+            </div>
+            <div class="shopping_controller">
+
+                <div class="shopping_price">
+                    <div class="shopping_nowprice">
+
+                    </div>
+                    <div class="shopping_rawprice">
+
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</template>
